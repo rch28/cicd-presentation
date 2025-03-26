5b2d69d8ad9fd322bbefec6f1818a53afb6fdfcd
Raju Chhetri <rajuchhetry11@gmail.com>
2025-03-26 22:10:16 +0545

    updated config file

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/cicd-presentation/" : "",
 };
 
 export default nextConfig;
